2e71f6955275d5e6f537d390678f574d411c67d9
Jinme Mirabal <mirabalj@gmail.com>
2016-06-14 15:34:08 -0400

    Change routing views to AngularJS ng-route

--- app/controllers/movieSwap.client.js
+++ app/controllers/movieSwap.client.js
@@ -1,2 +1,17 @@
 // Define the app module
 var movieswApp = angular.module('movieswApp', ['ngRoute']);
+// Define the configuration of app
+movieswApp.config(['$locationProvider' ,'$routeProvider',
+  function config($locationProvider, $routeProvider) {
+    // Define routes
+    $routeProvider.
+      when('/', {
+        templateUrl: '/views/main.html'
+      }).
+      otherwise('/');
+  }
+]);
+// Define the main controller on the app module
+movieswApp.controller('mainController', function mainController($scope, $http) {
+  $scope.object = "movies";
+});
