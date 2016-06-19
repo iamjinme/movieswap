31893f0755c457f8303a40b0fb680d56dbcdcd3d
Jinme Mirabal <mirabalj@gmail.com>
2016-06-19 14:51:44 -0400

    Assign true/false to logged var, as the case

--- app/controllers/sessionService.client.js
+++ app/controllers/sessionService.client.js
@@ -6,10 +6,12 @@ movieswApp.factory('session', function(){
 
   session.set = function(name, value) {
     session[name] = value;
+    session.logged = session.user.hasOwnProperty('_id');
   };
 
   session.remove = function() {
     session.user = {};
+    session.logged = false;
   }
 
   return session;
