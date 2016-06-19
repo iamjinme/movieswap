c5b8214517d028806df0ae9f5c357eca04caeebc
Jinme Mirabal <mirabalj@gmail.com>
2016-06-19 15:23:55 -0400

    Validate user in view for trading movies

--- app/controllers/allController.client.js
+++ app/controllers/allController.client.js
@@ -1,7 +1,7 @@
 // All Movies Controller
 movieswApp.controller('allController', function allController($scope, $http, session) {
   $scope.logged = session.logged;
-  console.log(session.user.hasOwnProperty('_id'), session);
+  $scope.user_id = $scope.logged ? session.user._id : null;
   $scope.collection = [];
   // Load Movies
   $http.get('/api/movies')
