4b3bd078a5665bc75f4a34312a96fcd67af18e15
Jinme Mirabal <mirabalj@gmail.com>
2016-06-23 17:54:47 -0400

    Delete unnecesary methods. Add info and credits to main view

--- app/controllers/movieSwap.server.js
+++ app/controllers/movieSwap.server.js
@@ -175,7 +175,7 @@ function MovieSwap () {
 		Movie
 	  	.find({}, { __v: false })
 	    .sort({'date': -1})
-			.limit(18)
+			.limit(60)
 	    .exec(function(err, movies) {
 	    	res.json(movies);
 	   	});
@@ -358,38 +358,6 @@ function MovieSwap () {
 			res.json({ error: true, message: 'Unauthorized'});
 		}
 	}
-
-	this.getClicks = function (req, res) {
-		Users
-			.findOne({ 'github.id': req.user.github.id }, { '_id': false })
-			.exec(function (err, result) {
-				if (err) { throw err; }
-
-				res.json(result.nbrClicks);
-			});
-	};
-
-	this.addClick = function (req, res) {
-		Users
-			.findOneAndUpdate({ 'github.id': req.user.github.id }, { $inc: { 'nbrClicks.clicks': 1 } })
-			.exec(function (err, result) {
-					if (err) { throw err; }
-
-					res.json(result.nbrClicks);
-				}
-			);
-	};
-
-	this.resetClicks = function (req, res) {
-		Users
-			.findOneAndUpdate({ 'github.id': req.user.github.id }, { 'nbrClicks.clicks': 0 })
-			.exec(function (err, result) {
-					if (err) { throw err; }
-
-					res.json(result.nbrClicks);
-				}
-			);
-	};
 
 }
 
